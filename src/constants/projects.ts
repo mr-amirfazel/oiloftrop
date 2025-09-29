ba4393f28fc2d3ee07c1966b8c291f86742e0df5
Fazel <koozegar2000@gmail.com>
2025-09-30 00:59:22 +0330

    [feat]: added project component and data constats

---
+++ src/constants/projects.ts
@@ -0,0 +1,101 @@
+import gabe from '../assets/images/gabe.png';
+import bamboo from '../assets/images/bamboo.png';
+import pomodoro from '../assets/images/pomodoro.png';
+import sortingVisualizer from '../assets/images/sortingVisualizer.png';
+import twitter from '../assets/images/X.png';
+import Tahcoin from '../assets/images/Tahcoin.png';
+import brobank from '../assets/images/brobank.jpeg';
+import rednif from '../assets/images/rednif.jpeg';
+import jykh from '../assets/images/jykh.jpeg';
+import p2p from '../assets/images/p2p.jpeg';
+import fuzzy from '../assets/images/fuzzy.jpeg';
+import cc from '../assets/images/cc.jpeg';
+import chisai from '../assets/images/chisai.jpeg';
+import clash from '../assets/images/clash.jpg';
+
+export const items = [
+    {
+        image: bamboo,
+        link: 'https://bamboo-five.vercel.app/',
+        title: 'Bamboo',
+        description: 'An online learning website resulted in a team work approach at Quera bootcamp'
+      },
+    {
+      image: gabe,
+      link: 'https://github.com/mr-amirfazel/Gabe',
+      title: 'Gabe',
+      description: 'A realtime messanger built with react, golang, nest js and socket.io'
+    },
+    {
+      image: pomodoro,
+      link: 'https://pomodoro-phi-eight.vercel.app/',
+      title: 'pomodoro',
+      description: 'A pomodoro timer to study efficiently'
+    },
+    {
+      image: sortingVisualizer,
+      link: 'https://sorting-visualizer-f1053.web.app/',
+      title: 'sorting visualizer',
+      description: 'A website to view how sorting algorithms work visually'
+    },
+    {
+        image: twitter,
+        link: 'https://twitter-clone-nine-phi.vercel.app/',
+        title: 'Twitter clone',
+        description: 'A small clone of X (formerly known as twitter)'
+    },
+    {
+        image: Tahcoin,
+        link: 'https://github.com/mr-amirfazel/tahcoin',
+        title: 'Tahcoin',
+        description: 'A simple mock of blockchain concept'
+    },
+    {
+        image: brobank,
+        link: 'https://github.com/mr-amirfazel/brobank',
+        title: 'Brobank',
+        description: 'A simple SAAS that uses cloud native services.'
+    },
+    {
+        image: rednif,
+        link: 'https://github.com/mr-amirfazel/rednif',
+        title: 'Rednif',
+        description: 'A simple search engine using tf-idf and champion list to retrieve data'
+    },
+    {
+        image: jykh,
+        link: 'https://github.com/mr-amirfazel/JYKH',
+        title: 'JYKH',
+        description: 'Yet another search engine using vector embedding and BERT to retreive data'
+    },
+    {
+        image: p2p,
+        link: 'https://github.com/mr-amirfazel/lilTorrent',
+        title: 'lilTorrent',
+        description: 'A small peer to peer network enabling nodes to send each other media.'
+    },
+    {
+        image: clash,
+        link: 'https://github.com/mr-amirfazel/AP-Final-Project',
+        title: 'Clash royale Clone',
+        description: 'A clash royale clone game as My Advanced Programming final course written in java'
+    },
+    {
+        image: fuzzy,
+        link: 'https://github.com/mr-amirfazel/CI_Fuzzy_systems_self_driving_car',
+        title: 'Fuzzy autonomous car',
+        description: 'A self driving car using fuzzy logic to find its path.'
+    },
+    {
+        image: cc,
+        link: 'https://aut-ce-cloud-computing.github.io/CC_website/',
+        title: 'Cloud Computing Course',
+        description: 'Website  designed for cloud computing course.'
+    },
+    {
+        image: chisai,
+        link: 'https://github.com/Euriground/chisai',
+        title: 'Chisai',
+        description: 'Simple URL shortner'
+    }
+  ];
--- src/pages/Desktop/projects/index.tsx
+++ src/pages/Desktop/projects/index.tsx
@@ -1,7 +1,13 @@
+import InfiniteMenu from "@/components/InfiniteMenu";
 import type { FC } from "react";
+
+import { items } from "@/constants/projects";
+
 
 export const Projects = () => {
     return (
-        <h2>Projects</h2>
+        <div className="w-full h-full text-white relative">
+            <InfiniteMenu items={items}/>
+        </div>
     )
 }
