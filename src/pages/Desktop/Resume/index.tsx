fac989ed7ba033400c2d2d53b3c03987ce2a350f
Fazel <koozegar2000@gmail.com>
2025-09-29 21:32:03 +0330

    [feat]: added pdf viewer for resume

--- src/pages/Desktop/Resume/index.tsx
+++ src/pages/Desktop/Resume/index.tsx
@@ -2,7 +2,7 @@ import { FC, useState } from "react";
 import { pdfjs } from 'react-pdf';
 import { Document, Page } from 'react-pdf';
 
-import resume from '../../../assets/pdf/rnd.pdf';
+import resume from '../../../assets/pdf/MyResume.pdf';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 // pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
@@ -16,13 +16,42 @@ export const Resume: FC = () => {
     }
   
     return (
-      <div>
+    //   <div>
+    //     <Document file={resume} onLoadSuccess={onDocumentLoadSuccess}>
+    //       <Page pageNumber={pageNumber} />
+    //     </Document>
+    //     <p>
+    //       Page {pageNumber} of {numPages}
+    //     </p>
+    //   </div>
+    <div className="relative w-full h-full flex flex-col items-center p-6">
+      <h1 className="text-3xl font-bold mb-4 text-yellow-400">My Resume</h1>
+
+      {/* Download button */}
+      <a
+        href={resume}
+        download
+        className="mb-6 px-6 py-2 rounded-xl bg-yellow-400 text-black font-semibold hover:bg-yellow-500 transition"
+      >
+        Download Resume
+      </a>
+
+      {/* PDF Viewer */}
+      <div className="flex flex-col gap-4 *:w-full max-w-fit bg-white shadow-lg rounded-xl border-amber-300 h-fit p-4">
         <Document file={resume} onLoadSuccess={onDocumentLoadSuccess}>
-          <Page pageNumber={pageNumber} />
+          {Array.from(new Array(numPages), (_el, index) => (
+            <div className="shadow-md rounded-md border-amber-300 mb-4 border-2">
+              <Page 
+            key={`page_${index + 1}`} 
+            pageNumber={index + 1} 
+            renderTextLayer={false} 
+            renderAnnotationLayer={false} 
+            className='p-2'
+          />
+            </div>
+          ))}
         </Document>
-        <p>
-          Page {pageNumber} of {numPages}
-        </p>
       </div>
+    </div>
     );
 };
